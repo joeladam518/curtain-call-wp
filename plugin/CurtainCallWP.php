dbd60fc0b04d1fe19e73d349c12a83dab3de617f
Joel Haker <joeladam@gmail.com>
2019-10-17 21:07:58 -0400

    Clean up the front facing plugin

--- plugin/CurtainCallWP.php
+++ plugin/CurtainCallWP.php
@@ -12,62 +12,36 @@
  * Domain Path:       /languages
 **/
 
-# If this file is called directly, abort.
 if (!defined('WPINC')) {
-    die;
+    die; // If this file is called directly, abort.
 }
 
-/**
- *  Define Constants
-**/
+// Plugin constants
 define('CCWP_VERSION', '0.1.0');
 define('CCWP_PLUGIN_NAME', 'CurtainCallWP');
 define('CCWP_TEXT_DOMAIN', 'curtain-call-wp');
 
-/**
- *  Load Composer
-**/
+// Load composer dependencies
 require_once 'vendor/autoload.php';
-require_once 'src/includes/CurtainCallDebugHelperFunctions.php';
 
+// Register the activation and deactivation hooks
+register_activation_hook(__FILE__, function() {
+	CurtainCallWP\includes\CurtainCallActivator::activate();
+});
+register_deactivation_hook(__FILE__, function() {
+	CurtainCallWP\includes\CurtainCallDeactivator::deactivate();
+});
 
-/**
- *  The code that runs during plugin activation.
- *  This action is documented in includes/CurtainCallActivator.php
-**/
-function activate_curtain_call_wp() {
-    CurtainCallWP\includes\CurtainCallActivator::activate();
-}
-
-/**
- *  The code that runs during plugin deactivation.
- *  This action is documented in includes/CurtainCallDeactivator.php
-**/
-function deactivate_curtain_call_wp() {
-    CurtainCallWP\includes\CurtainCallDeactivator::deactivate();
-}
-
-register_activation_hook(__FILE__, 'activate_curtain_call_wp');
-register_deactivation_hook(__FILE__, 'deactivate_curtain_call_wp');
-
-/**
- *  The core plugin class that is used to define internationalization,
- *  admin-specific hooks, and public-facing site hooks.
-**/
-//require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
-
-/**
- *  Begins execution of the plugin.
- *
- *  Since everything within the plugin is registered via hooks,
- *  then kicking off the plugin from this point in the file does
- *  not affect the page life cycle.
- *
- *  @since    0.0.1
-**/
+/*
+ | Begins execution of the plugin.
+ |
+ | Since everything within the plugin is registered via hooks,
+ | then kicking off the plugin from this point in the file does
+ | not affect the page life cycle.
+ */
 function run_curtain_call_wp() {
-    $plugin = new CurtainCallWP\CurtainCall();
-    $plugin->run();
+	$plugin = new CurtainCallWP\CurtainCall();
+	$plugin->run();
 }
 
 run_curtain_call_wp();
