2936cde198adb05b588a7eecc8f6c3b57fbe1ad9
Joel Haker <joeladam@gmail.com>
2019-11-02 18:42:22 -0400

    Change view class to use compile then render instead of render and then display

--- plugin/src/CurtainCallView.php
+++ plugin/src/CurtainCallView.php
@@ -43,13 +43,17 @@ class CurtainCallView
         return ccwp_plugin_path('src/Views/') . $path;
     }
     
-    public function render(): string
+    /**
+     * Compile the php template/partial into an html string
+     * @return string
+     */
+    protected function compile(): string
     {
         if (!file_exists($this->getTemplatePath())) {
             //TODO: Change this back to an empty string
             return '<h3 style="color:red;">Couldn\'t find view template path: "'. $this->getTemplatePath() .'"</h3>';
         }
-        
+    
         if (count($this->data) > 0) {
             if (!$this->validateDataArray()) {
                 //TODO: Change this back to an empty string
@@ -57,17 +61,27 @@ class CurtainCallView
             }
             extract($this->data);
         }
-        
+    
         ob_start();
-        
+    
         include $this->getTemplatePath();
-        
+    
         return ob_get_clean();
     }
-
-    public function display(): void
+    
+    /**
+     * Either return the compiled html string or echo it out
+     *
+     * @param bool $return
+     * @return string|void
+     */
+    public function render(bool $return = false)
     {
-        echo $this->render();
+        if ($return) {
+            return $this->compile();
+        }
+        
+        echo $this->compile();
     }
     
     public function getTemplatePath(): string
@@ -75,7 +89,7 @@ class CurtainCallView
         return $this->views_path . $this->file_path;
     }
     
-    private function validateDataArray(): bool
+    protected function validateDataArray(): bool
     {
         // Checks to see if $this->data is an associative array;
         return array_keys($this->data) !== range(0, count($this->data) - 1);
