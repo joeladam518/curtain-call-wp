4573531d9f885a2cf3ef6ef3544630634fa776f3
Joel Haker <joeladam@gmail.com>
2019-11-28 10:43:09 -0500

    Add function for determining template

--- plugin/src/Hooks/FrontendHookController.php
+++ plugin/src/Hooks/FrontendHookController.php
@@ -43,19 +43,36 @@ class FrontendHookController extends CurtainCallHookController
     }
     
     /**
-     * @global $post
-     * @param $template
-     * @return string
+     * Determine if the current theme has a ccwp post type template
+     * @param string $type
+     * @param array  $templates
+     * @return bool
      */
-    public function loadSingleTemplates($template)
+    private function themeHasTemplate(string $type, array $templates): bool
+    {
+        $theme_template = locate_template(array_filter($templates, function($item) use($type) {
+            return $item !== $type.'.php';
+        }));
+        
+        return !empty($theme_template);
+    }
+    
+    /**
+     * @param  string  $template
+     * @param  string  $type
+     * @param  array   $templates
+     * @return string
+     * @global WP_Post $post
+     */
+    public function loadSingleTemplates($template, $type, $templates)
     {
         global $post;
         
-        if ($post->post_type == 'ccwp_production' && $template !== locate_template(['single-ccwp_production.php'])) {
+        if ($post->post_type === 'ccwp_production' && !$this->themeHasTemplate($type, $templates)) {
             return CurtainCallView::dirPath('frontend/') . '/templates/single-ccwp_production.php';
         }
         
-        if ($post->post_type == 'ccwp_cast_and_crew' && $template !== locate_template(['single-ccwp_cast_and_crew.php'])) {
+        if ($post->post_type === 'ccwp_cast_and_crew' && !$this->themeHasTemplate($type, $templates)) {
             return CurtainCallView::dirPath('frontend') . '/templates/single-ccwp_cast_and_crew.php';
         }
         
@@ -63,19 +80,18 @@ class FrontendHookController extends CurtainCallHookController
     }
     
     /**
-     * @global $post
-     * @param $template
+     * @param  string  $template
+     * @param  string  $type
+     * @param  array   $templates
      * @return string
      */
-    public function loadArchiveTemplates($template)
+    public function loadArchiveTemplates($template, $type, $templates)
     {
-        global $post;
-        
-        if (is_post_type_archive('ccwp_production') && $template !== locate_template(['archive-ccwp_production.php'])) {
+        if (is_post_type_archive('ccwp_production') && !$this->themeHasTemplate($type, $templates)) {
             return CurtainCallView::dirPath('frontend/') . 'templates/archive-ccwp_production.php';
         }
         
-        if (is_post_type_archive('ccwp_cast_and_crew') && $template !== locate_template(['archive-ccwp_cast_and_crew.php'])) {
+        if (is_post_type_archive('ccwp_cast_and_crew') && !$this->themeHasTemplate($type, $templates)) {
             return CurtainCallView::dirPath('frontend/') . 'templates/archive-ccwp_cast_and_crew.php';
         }
         
