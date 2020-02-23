b6b83f0dab52210cf56f7801b99bb6029f429f11
Joel Haker <joeladam@gmail.com>
2020-02-23 11:37:30 -0500

    Adds order to each query

--- plugin/src/PostTypes/Traits/QueriesWordPressForProductions.php
+++ plugin/src/PostTypes/Traits/QueriesWordPressForProductions.php
@@ -18,51 +18,8 @@ trait QueriesWordPressForProductions
         'post_status' => 'publish',
         'meta_key' => '_ccwp_production_date_start',
         'orderby' => 'meta_value',
-        'order'   => 'ASC',
         'nopaging' => true,
     ];
-    
-    protected static function getCurrentQueryArgs(): array
-    {
-        $today = static::getTodaysDate();
-        
-        return array_merge(static::$wp_query_args, [
-            'meta_query' => [
-                'relation' => 'AND',
-                [
-                    'key' => '_ccwp_production_date_start',
-                    'value' => $today,
-                    'compare' => '<=',
-                ],
-                [
-                    'key' => '_ccwp_production_date_end',
-                    'value' => $today,
-                    'compare' => '>=',
-                ]
-            ]
-        ]);
-    }
-    
-    protected static function getFutureQueryArgs(): array
-    {
-        $today = static::getTodaysDate();
-
-        return array_merge(static::$wp_query_args, [
-            'meta_query' => [
-                'relation' => 'AND',
-                [
-                    'key' => '_ccwp_production_date_start',
-                    'value' => $today,
-                    'compare' => '>',
-                ],
-                [
-                    'key' => '_ccwp_production_date_end',
-                    'value' => $today,
-                    'compare' => '>',
-                ]
-            ]
-        ]);
-    }
     
     protected static function getPastQueryArgs(): array
     {
@@ -86,6 +43,55 @@ trait QueriesWordPressForProductions
         ]);
     }
     
+    protected static function getCurrentQueryArgs(): array
+    {
+        $today = static::getTodaysDate();
+        
+        return array_merge(static::$wp_query_args, [
+            'order' => 'ASC',
+            'meta_query' => [
+                'relation' => 'AND',
+                [
+                    'key' => '_ccwp_production_date_start',
+                    'value' => $today,
+                    'compare' => '<=',
+                ],
+                [
+                    'key' => '_ccwp_production_date_end',
+                    'value' => $today,
+                    'compare' => '>=',
+                ]
+            ]
+        ]);
+    }
+    
+    protected static function getFutureQueryArgs(): array
+    {
+        $today = static::getTodaysDate();
+
+        return array_merge(static::$wp_query_args, [
+            'order' => 'ASC',
+            'meta_query' => [
+                'relation' => 'AND',
+                [
+                    'key' => '_ccwp_production_date_start',
+                    'value' => $today,
+                    'compare' => '>',
+                ],
+                [
+                    'key' => '_ccwp_production_date_end',
+                    'value' => $today,
+                    'compare' => '>',
+                ]
+            ]
+        ]);
+    }
+    
+    public static function getPastPosts(): WP_Query
+    {
+        return new WP_Query(static::getPastQueryArgs());
+    }
+    
     public static function getCurrentPosts(): WP_Query
     {
         return new WP_Query(static::getCurrentQueryArgs());
@@ -94,10 +100,5 @@ trait QueriesWordPressForProductions
     public static function getFuturePosts(): WP_Query
     {
         return new WP_Query(static::getFutureQueryArgs());
-    }
-    
-    public static function getPastPosts(): WP_Query
-    {
-        return new WP_Query(static::getPastQueryArgs());
     }
 }
