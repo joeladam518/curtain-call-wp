8566169afd1ab890c5ddb7cae8ef32f027f1bc62
Joel Haker <joeladam@gmail.com>
2021-11-24 09:19:52 -0500

    rename file

--- plugin/CurtainCall/Hooks/GlobalHooks.php
+++ plugin/CurtainCall/Hooks/GlobalHooks.php
@@ -5,24 +5,11 @@ namespace CurtainCallWP\Hooks;
 use CurtainCallWP\PostTypes\CastAndCrew;
 use CurtainCallWP\PostTypes\Production;
 
-/**
- * Class CurtainCallController
- * @package CurtainCallWP\Controllers
- */
-class CurtainCallHooks
+class GlobalHooks
 {
     /**
-     * Register the production custom post type.
-     * @return void
-     */
-    public function createProductionPostType()
-    {
-        register_post_type('ccwp_production', Production::getConfig());
-        flush_rewrite_rules();
-    }
-    
-    /**
-     * Register the cast/crew custom post type.
+     * Register the cast/crew custom post type
+     *
      * @return void
      */
     public function createCastAndCrewPostType()
@@ -30,9 +17,21 @@ class CurtainCallHooks
         register_post_type('ccwp_cast_and_crew', CastAndCrew::getConfig());
         flush_rewrite_rules();
     }
-    
+
     /**
-     * Create the production custom post type taxonomies.
+     * Register the production custom post type
+     *
+     * @return void
+     */
+    public function createProductionPostType()
+    {
+        register_post_type('ccwp_production', Production::getConfig());
+        flush_rewrite_rules();
+    }
+
+    /**
+     * Create the production custom post type taxonomies
+     *
      * @return void
      */
     public function createProductionSeasonsTaxonomy()
@@ -40,13 +39,14 @@ class CurtainCallHooks
         register_taxonomy('ccwp_production_seasons', array('ccwp_production'), Production::getSeasonsTaxonomyConfig());
         flush_rewrite_rules();
     }
-    
+
     /**
      * View rewrite rules (for debugging)
+     *
      * @param array $rules
      * @return array
      */
-    public function filterRewriteRulesArray($rules)
+    public function filterRewriteRulesArray($rules): array
     {
         //pr($rules,1);
         return $rules;
