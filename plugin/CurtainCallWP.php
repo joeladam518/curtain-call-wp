ec0b0f764b15431674cae842fbdf7745a792d646
Joel Haker <joeladam@gmail.com>
2021-11-24 09:28:37 -0500

    Move start of the the execution of the plugin to a static function

--- plugin/CurtainCallWP.php
+++ plugin/CurtainCallWP.php
@@ -12,31 +12,13 @@
  * Domain Path:  CurtainCall/languages
 **/
 
-use CurtainCallWP\CurtainCall;
-
-// Plugin constants
 define('CCWP_PLUGIN_NAME', 'CurtainCallWP');
 define('CCWP_PLUGIN_VERSION', '0.4.1');
 define('CCWP_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('CCWP_TEXT_DOMAIN', 'curtain-call-wp');
 define('CCWP_DEBUG', false);
 
-// Load dependencies
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 
-// Register Plugin Life Cycle Hooks
-CurtainCall::registerLifeCycleHooks(__FILE__);
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
-**/
-function ccwp_run_plugin()
-{
-    $ccwp_plugin = new CurtainCall();
-    $ccwp_plugin->run();
-}
-ccwp_run_plugin();
+\CurtainCallWP\CurtainCall::registerLifeCycleHooks(__FILE__);
+\CurtainCallWP\CurtainCall::run();
