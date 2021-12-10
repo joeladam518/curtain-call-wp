2475951e6748a5594fe956ec0b4af3f72d2046e3
Joel Haker <joeladam@gmail.com>
2021-12-10 00:16:15 -0500

    space settings page

--- plugin/views/admin/settings-page.php
+++ plugin/views/admin/settings-page.php
@@ -11,15 +11,15 @@ settings_errors('ccwp_messages');
     <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
 
     <form action="options.php" method="post">
-    <?php
-        // output security fields for the registered setting
-        settings_fields('ccwp-settings');
+        <?php
+            // output security fields for the registered setting
+            settings_fields('ccwp-settings');
 
-        // output setting sections and their fields
-        do_settings_sections('ccwp');
+            // output setting sections and their fields
+            do_settings_sections('ccwp');
 
-        // output save settings button
-        submit_button('Save');
-    ?>
+            // output save settings button
+            submit_button('Save');
+        ?>
     </form>
 </div>
