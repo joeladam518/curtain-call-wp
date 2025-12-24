cd67f752018851174a9ba5af8888bfc9074a0f83
Joel Haker <joeladam@gmail.com>
2025-12-24 03:21:34 -0500

    add the data attached to window

---
+++ resources/js/ccwp.d.ts
@@ -0,0 +1,17 @@
+import MemberType from './enums/MemberType';
+import {CastCrewDetails, CastCrewProduction, ProductionCastCrew, ProductionDetails} from './types/metaboxes';
+
+declare global {
+    interface Window {
+        CCWP_DATA?: {
+            initialDetails?: ProductionDetails | CastCrewDetails | null | undefined;
+            options?: ({label: string, value: string})[];
+            cast?: ProductionCastCrew<MemberType.Cast>[];
+            crew?: ProductionCastCrew<MemberType.Crew>[];
+            productions?: CastCrewProduction[];
+        } | null | undefined;
+        CCWP_SETTINGS?: {
+            nonce: string;
+        } | null | undefined;
+    }
+}
