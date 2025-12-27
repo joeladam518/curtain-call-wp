03a2fb0baddea6e2b2a029ce080e51a07d0c8175
Joel Haker <joeladam@gmail.com>
2025-12-27 15:27:45 -0500

    update dateToFormat function

--- resources/js/utils/dates.ts
+++ resources/js/utils/dates.ts
@@ -1,36 +1,64 @@
-import {format, formatISO, type FormatOptions, isDate, isValid, parse, toDate} from 'date-fns';
+import {
+    format,
+    formatISO,
+    type FormatOptions,
+    isDate,
+    isValid,
+    parse as parseFns,
+    parseISO,
+    type ParseOptions,
+    toDate
+} from 'date-fns';
 
 type DateToFormatInputValue = Date | string | null | undefined;
 type DateToFormatOptions = {
     input?: string,
     output?: string,
     formatOptions?: FormatOptions
+    parseOptions?: ParseOptions
 }
 
-export function dateToFormat(value: Date | null | undefined, format?: string): string | undefined
-export function dateToFormat(value: DateToFormatInputValue, options?: DateToFormatOptions): string | undefined
-export function dateToFormat(value: DateToFormatInputValue, options?: DateToFormatOptions | string): string | undefined {
+export function dateToFormat(
+    value: Date | null | undefined,
+    format?: string
+): string | undefined
+export function dateToFormat(
+    value: DateToFormatInputValue,
+    options?: DateToFormatOptions
+): string | undefined
+export function dateToFormat(
+    value: DateToFormatInputValue,
+    options?: DateToFormatOptions | string
+): string | undefined {
     if (!value) {
         return undefined;
     }
 
     const {
-        input = 'yyyy-MM-dd',
-        output = 'yyyy-MM-dd',
+        input,
+        output,
+        parseOptions,
         formatOptions,
     } = ((typeof options === 'string' ? {output: options} : options) ?? {});
 
     const date = isDate(value)
         ? toDate(value)
-        : parse(value, input, new Date(0, 0, 0, 0, 0, 0, 0));
+        : input
+            ? parse(value, input, parseOptions)
+            : parseISO(value, parseOptions);
 
     if (!isValid(date)) {
         return undefined;
     }
 
-    if (output === 'iso') {
+    if (!output || output === 'iso') {
         return formatISO(date, formatOptions);
     }
 
     return format(date, output, formatOptions);
+}
+
+export function parse(dateStr: string, format: string, options?: ParseOptions): Date {
+    const now = new Date();
+    return parseFns(dateStr, format, new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0), options);
 }
